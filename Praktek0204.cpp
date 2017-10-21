416b024a8122616929735c7770b3e4dcac88ec45
HarisIrwansyah <haris.irwansyah4@gmail.com>
2017-10-21 09:12:13 +0800

    Menghitung dan menampilkan biaya parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -6,9 +6,14 @@ int main () {
 	double parket=193*1215;
 	double luasBox = parket*8/1000000;
 	
-	cout << "Luas Rumah		: "<<luasRmh<<" m2"<<endl;
+	cout << "Luas Rumah 	: "<<luasRmh<<" m2"<<endl;
 	cout << "Luas 1 Box Parket	: "<<luasBox<<" m2"<<endl;
 	
 	double jumlahBox, bayarParket, bayarPasang, totalBayar;
+	jumlahBox = luasRmh/luasBox;
+	cout <<"jumlah Box Parket 	: "<<jumlahBox<<endl;
+	
+	bayarParket = jumlahBox*500;
+	cout <<"Biaya Parket (Rp) 	: "<<bayarParket<<endl;
 	
 }
