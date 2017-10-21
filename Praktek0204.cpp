582b95aec2ad9a6b8f7de0c067483126bb1d8787
HarisIrwansyah <haris.irwansyah4@gmail.com>
2017-10-21 08:56:33 +0800

    input nilai luas rumah dan luas 1 box

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -6,4 +6,7 @@ int main () {
 	double parket=193*1215;
 	double luasBox = parket*8/1000000;
 	
+	cout << "Luas Rumah		: "<<luasRmh<<" m2"<<endl;
+	cout << "Luas 1 Box Parket	: "<<luasBox<<" m2"<<endl;
+	
 }
